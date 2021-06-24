5a9f7ee7b352da897162080a7d021d4c43ddfb73
unknown <shmoon@ssoltech.io>
2021-06-24 11:38:59 +0900

    second commit

---
+++ src/test/java/com/ssoltech/test/test_springboot_docker_image/TestSpringBootDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoltech.test.test_springboot_docker_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootDockerImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
